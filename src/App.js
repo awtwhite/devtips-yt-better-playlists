6846a23c03473bf47de164023dc89b729b879d1a
Andrew White <awtwhite@gmail.com>
2018-11-24 17:52:02 +1000

    Update styles and add some fake data

--- src/App.js
+++ src/App.js
@@ -1,74 +1,102 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-let defaultColor = '#fff';
 let defaultStyle = {
-    color: defaultColor
+  color: '#fff'
 };
 let otherStyle = {
-    background: 'tomato'
+  background: 'tomato'
 };
+let fakeServerData = {
+  user: {
+    name: 'Andrew',
+    playlists: [
+      {
+        name: 'Favourites',
+        songs: ['Starcity', 'Many plow', 'Serpent sole']
+      }, {
+        name: 'Discover weekly',
+        songs: ['Hamony', 'Sandlewood', 'Tri state']
+      }, {
+        name: 'Moto',
+        songs: ['Floss nogo', 'Anywhoo', 'Bass sol']
+      }, {
+        name: 'Gaming',
+        songs: ['Good god', 'Kitty', 'Only once']
+      }
+    ]
+  }
+}
 
 class Aggregate extends Component {
   render() {
-    return (
-      <div style={{
-          ...defaultStyle,
-          display: 'inline-block'
-      }}>
-          <h2>Number text</h2>
-      </div>
-    );
+    return (<div style={{
+        ...defaultStyle,
+        display: 'inline-block'
+    }}>
+      <h2>
+        {this.props.playlists && this.props.playlists.length} text
+      </h2>
+    </div>);
   }
 }
 
 class Filter extends Component {
-render() {
-  return (
-     <div style={defaultStyle}>
-         <img />
-         <input type="text"></input>
-         Filter
-     </div>
-    );
+  render() {
+    return (<div className='app' style={defaultStyle}>
+      <img/>
+      <input type="text"></input>
+      Filter
+    </div>);
   }
 }
 
 class Playlist extends Component {
-    render() {
-        return (
-           <div style={{
-               ...defaultStyle,
-               display: 'inline-block',
-               width: "25%"
-           }}>
-                <img />
-                <h3>Playlist name</h3>
-                <ul>
-                    <li>Song name 1</li>
-                    <li>Song name 2</li>
-                    <li>Song name 3</li>
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    return (<div style={{
+        ...defaultStyle,
+        display: 'inline-block',
+        width: "25%"
+      }}>
+      <img/>
+      <h3>Playlist name</h3>
+      <ul>
+        <li>Song name 1</li>
+        <li>Song name 2</li>
+        <li>Song name 3</li>
+      </ul>
+    </div>);
+  }
 }
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {serverData: {}}
+  }
+  componentDidMount() {
+    this.setState({serverData: fakeServerData})
+  }
   render() {
-    return (
-      <div className="App">
-          <h1>Better Playlist</h1>
-          <Aggregate />
-          <Aggregate />
-          <Filter />
-          <Playlist />
-          <Playlist />
-          <Playlist />
-          <Playlist />
-      </div>
-    );
+    return (<div className='App'>
+      <h1>
+        {this.state.serverData.user &&
+        this.state.serverData.user.name}'s Playlist
+      </h1>
+      <Aggregate
+        playlists={
+          this.state.serverData.user &&
+          this.state.serverData.user.playlists
+        }
+      />
+      <Aggregate/>
+      <Filter/>
+      <Playlist/>
+      <Playlist/>
+      <Playlist/>
+      <Playlist/>
+    </div>);
   }
 }
 
